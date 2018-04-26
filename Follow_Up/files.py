8d64d264826b0597fad88ce20300a7f5c7cbff7d
ST <sztuzel@gmail.com>
2018-04-27 00:10:24 +0200

    Update files.py

--- Follow_Up/files.py
+++ Follow_Up/files.py
@@ -1,16 +1,22 @@
 
 def write_powers_of_n_to_file(n):
+    '''
+    Given number n, open 'file.txt' for writing and write to it powers of n
+    '''
     pass
 
-
+# same as above, but use 'with' clause to open a file 
 def improved_write(n):
     pass
 
 
 def read_file_and_print(filename):
+    '''
+    read given file filename and print the content to the console
+    '''
     pass
 
-
+# same as above, but print only every second line
 def read_file_and_print_every_second_line(filename):
     pass
             
